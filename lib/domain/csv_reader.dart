3266059929277e51952b582f04603979735bbea5
Max <weber.max.1988@live.de>
2021-09-28 18:23:02 +0200

    [Feature] Improved and hardend our csvReader

--- lib/domain/csv_reader.dart
+++ lib/domain/csv_reader.dart
@@ -1,11 +1,32 @@
 import 'dart:io';
 
 class CSVReader {
-  List<List<String>> readLessonFrom(File file) {
-    if (!file.path.endsWith(".csv")) throw UnsupportedError("Only csv is currently supported");
+  Map<String, String> readLessonFrom(File file) {
+    if (!file.path.endsWith(".csv")) {
+      throw UnsupportedError("Only csv is currently supported");
+    }
 
     final rows = file.readAsLinesSync();
-    List<List<String>> table = rows.map((String row) => row.split(',')).toList();
-    return table;
+
+    if (rows.isEmpty) {
+      throw UnsupportedError(
+          "The csv is empty, please provide at least 1 header and one q&a");
+    }
+
+    Map<String, String> questionAnswerMap = {};
+
+    for (var row in rows) {
+      var split = row.split(',');
+
+      if (split.length != 2) {
+        throw UnsupportedError("The csv must have exactly 2 columns");
+      }
+
+      questionAnswerMap.putIfAbsent(split[0], () {
+        return split[1];
+      });
+    }
+
+    return questionAnswerMap;
   }
 }
--- test/domain/csv_reader_test.dart
+++ test/domain/csv_reader_test.dart
@@ -7,13 +7,32 @@ void main() {
   group("CSV Reader", () {
     CSVReader subject = CSVReader();
 
-    test("WHEN we pass a csv file into the method THEN it should return a table", () {
+    test(
+        "WHEN we pass a csv file into the method THEN it should return a table",
+        () {
       File file = File("test/test_data/learn english/lesson_1.csv");
-      expect(subject.readLessonFrom(file)[1][1], "Hello");
+      var result = subject.readLessonFrom(file);
+      expect(result.containsValue("Hello"), true);
     });
 
-    test("WHEN we not pass a csv file into the method THEN it should throw", () {
+    test("WHEN we not pass a csv file into the method THEN it should throw",
+        () {
       File file = File("test/domain/csv_reader_test.dart");
+      expect(() => subject.readLessonFrom(file), throwsUnsupportedError);
+    });
+
+    test("WHEN we pass a csv file with three columns THEN it should throw", () {
+      File file = File("test/test_data/learn english/three_column.csv");
+      expect(() => subject.readLessonFrom(file), throwsUnsupportedError);
+    });
+
+    test("WHEN we pass an empty csv THEN it should throw", () {
+      File file = File("test/test_data/learn english/empty.csv");
+      expect(() => subject.readLessonFrom(file), throwsUnsupportedError);
+    });
+
+    test("WHEN we pass an one csv THEN it should throw", () {
+      File file = File("test/test_data/learn english/one_column.csv");
       expect(() => subject.readLessonFrom(file), throwsUnsupportedError);
     });
   });
