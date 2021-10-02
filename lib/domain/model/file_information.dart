8a8885bfc03bf70b6744a41e3c78b8ca1182588d
Max <weber.max.1988@live.de>
2021-10-02 08:36:13 +0200

    [Feature] Folder are now visible on top files, created a small tree and added the lessons below

--- lib/domain/model/file_information.dart
+++ lib/domain/model/file_information.dart
@@ -1,6 +1,13 @@
-class FileInformation {
+class LectureInformation {
   final String name;
   final String id;
 
-  FileInformation(this.name, this.id);
+  LectureInformation(this.name, this.id);
+}
+
+class LectureFolder {
+  final String name;
+  final List<LectureInformation> spreadsheets;
+
+  LectureFolder(this.name, this.spreadsheets);
 }
