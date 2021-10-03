867e606f56257200a98b1dbc4783f2dfdae77ce0
Max <weber.max.1988@live.de>
2021-10-03 16:24:45 +0200

    🎨 [Design] Improved the Lecture Selector to improve usability.

--- lib/design_system.dart
+++ lib/design_system.dart
@@ -1,6 +1,15 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
 class Insets {
   static const double small = 4.0;
   static const double medium = 8.0;
   static const double large = 12.0;
   static const double xtraLarge = 16.0;
+}
+
+// TODO: Replace by a proper Theme
+class FlashcardAppColors {
+  static const Color green = Colors.green;
 }
